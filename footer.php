667944ba573278422fbaad4954ab3bd611d1b86a
Rohan Shah <rohanshah2711@gmail.com>
2020-06-06 15:30:04 +0530

    Release on github

---
+++ footer.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style>
+#container{
+	  margin:0;
+	 padding:0;
+   }
+   body{
+    font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
+	 margin:0;
+	 padding:0;
+	 background-color:#4b6584;
+  }
+#footer_div {
+	position:absolute;
+  background-color:#4b6584;
+  width:100%;
+	height:648px;
+	z-index:1;
+}
+
+#fo1 {
+	position:absolute;
+	width:409px;
+	height:131px;
+	z-index:2;
+	left: 30px;
+	top: 23px;
+}
+#page_link_div {
+	position:absolute;
+	width:283px;
+	height:270px;
+	z-index:1;
+	left: 478px;
+	top: 67px;
+}
+#dept_div {
+	position:absolute;
+	width:283px;
+	height:115px;
+	z-index:2;
+	left: 875px;
+	top: 66px;
+}
+#para_div {
+	position:absolute;
+	width:350px;
+	height:216px;
+	z-index:3;
+	left: 32px;
+	top: 174px;
+}
+#footer_name_div {
+	position:absolute;
+	width:510px;
+	height:43px;
+	z-index:3;
+	left: 370px;
+	top: 552px;
+}
+#border_div {
+	position:absolute;
+	border-bottom:2px solid #5F7580;
+	width:100%;
+	height:2px;
+	z-index:4;
+	left: 0px;
+}
+#fblogo {
+	position:absolute;
+	width:25px;
+	height:23px;
+	left: 6px;
+	top: 190px;
+}
+#twlogo{
+	position:absolute;
+	width:25px;
+	height:23px;
+	left: 37px;
+	top: 190px;
+}
+#gpluslogo {
+	position:absolute;
+	width:25px;
+	height:23px;
+	left: 102px;
+	top: 190px;
+}
+#linklogo {
+	position:absolute;
+	width:25px;
+	height:23px;
+	left: 68px;
+ 	top: 190px;
+}
+.color_{
+	color:#999;
+}
+.color_white{
+	color:white;
+}
+</style>
+</head>
+
+<body >
+<div id="body">
+<div id="container">
+  <div id="footer_div">
+
+
+    <div id="para_div">
+      <table width="100%" height="113" border="0">
+        <tr>
+          <td><span class="color_"><i class="fa fa-map-marker" style="color:orange" >&nbsp;</i>A.P Institute of Technology.Shah,kasarwadavali, Thane, India.</span></td>
+        </tr>
+        <tr>
+					<td><span class="color_"><i class onloadedmetadata="Maharashtra" draggable="auto" role="button" >How to Reach Us</i></span></td>
+
+          <td><span class="color_"><i class="fa fa-envelope" style="color:orange" >&nbsp;</i>(+91) 123 456 7896</span></td>
+        </tr>
+        <tr>
+          <td><span class="color_"><i class="fa fa-phone" style="color:orange" >&nbsp;</i>apsit.org.in</span></td>
+        </tr>
+      </table>
+      <p>&nbsp;</p>
+      <p> <div id="fblogo"><i class="fa fa-facebook" style="font-size:12px ;color:white"></i></div>
+      <div id="twlogo"> <i class="fa fa-twitter" style="color:white"></i> </div>
+      <div id="gpluslogo"> <i class="fa fa-google-plus" style="color:white"></i> </div>
+      <div id="linklogo"> <i class="fa fa-linkedin" style="color:white"></i> </div></p>
+      </div>
+      <div id="border_div"></div>
+      </div>
+        <div id="footer_name_div">
+            <h4 class="color_">Copyright 2017 © Theme Created by Dumbheads.</h4>
+        </div>
+
+<div>
+	<h5> Copyright 2017 Theme Developed by Dumbheads</h5>
+</div>
+</div>
+</div>
+<div id="fot_div">
+	<?php include 'footer.php';?></div>
+</body>
+</html>
